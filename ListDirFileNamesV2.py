8cd49e9b8b777880a9dd7d07e6705ca591e4cdc6
homerflander <van.vincent2013@gmail.com>
2017-07-10 23:43:23 -0700

    V2.0.1 Tweaked for performance and bugs

--- ListDirFileNamesV2.py
+++ ListDirFileNamesV2.py
@@ -1,54 +1,49 @@
 import os
 import sys
+import io
 
 def search(scriptparam,depth):
-    templist=""
     dirList = os.listdir("./")
     for filename in dirList:
         x=0
         holder=""
         for x in range (0,depth):
-            holder=holder+"    "
+            holder=holder+"\t"
         if(depth>0):
             holder = holder + "->" + filename
         else:
             holder=holder+filename
-        print(holder)
         #print(depth) #was used for troubleshooting depth of subfiles/folders
         if (filename != scriptparam):
-            templist = templist + holder + "\n"
+            print(holder)
+            FILEOUT.write(holder + "\n")
             if os.path.isdir(filename):
                 #print(filename + " is a folder") #was used from troubleshooting if file was a folder or file
                 os.chdir(filename)
                 depth=depth+1
-                templist=templist+search(scriptparam,depth)
+                search(scriptparam, depth)
                 depth=depth-1
                 os.chdir("..")
-    return templist
 
 if len(sys.argv) is not 2:#takes in one argument for output textfile.
     sys.exit("Error, script needs one command-line argument. (output.txt File)")
 
 start=0#start of python script location string
 end=len(sys.argv[0])#end of python script location string
-list=""  # set up output name list
-#print(start)
-#print(end)
+directorylist=""  # set up output name directorylist
+#print(start) used for troubleshooting
+#print(end) used for troubleshooting
 
-#find python script name
+#find python script name, not entire location
 while(sys.argv[0].find("/",start,end)!=-1):
     #print(sys.argv[0].find("/",start,end))
     start=sys.argv[0].find("/",start,end)+1
 
 pscript=sys.argv[0][start:end]
-#create file name list excluding python script name
-list=search(pscript,0)
 
-print("\n")
-print("Now writing the following list of file names to %s" %sys.argv[1])
-print(list)
-
-#open output text file and write the file name list. Then close output file.
-FILEOUT = open(sys.argv[1], 'w')
-FILEOUT.write(list)
+#copen output.txt file to write the list, excluding the python script name
+FILEOUT=io.open(sys.argv[1], "w", encoding='utf-8')
+search(pscript,0)
 FILEOUT.close()
+
+
